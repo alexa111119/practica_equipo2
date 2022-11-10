10dda283c9b6ada4b8825ca276b393da7a7091ec
yli18 <2190021@ipisa.edu.do>
2022-11-10 08:13:18 -0400

    Cambios en el formularioVanc.css

--- formularioVANC.php
+++ formularioVANC.php
@@ -4,63 +4,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulario de vacantes</title>
     <link rel="stylesheet" type="text/css" href="css/formularioVANC.css">
 </head>
+
 <body>
+    
+<section class="form-register">
 
 <form action=" " method="post">
  <ul>
   <li>
     <label>Nombre de la empresa:</label>
     <input type="text" id="name" name="nombre_empresa">
-  </li>
+  </li> <br>  
 
   
   <li>
     <label>Nombre de la vacante:</label>
     <input type="text" id="name" name="vacante_nombre">
-  </li>
+  </li> <br>
 
   <li>
-    <label>Funciones o perfil del puesto:</label>
-    <textarea id="h" name="perfil_puesto"></textarea>
-    
-  </li>
+    Funciones o perfil del puesto: <br>
+    <textarea  COLS="40" ROWS="4" id="h" name="perfil_puesto"></textarea>
+  </li> <br>
 
   <li>
     <label>Sueldo:</label>
     <input type="text" id="name" name="sueldo">
-  </li>
+  </li> <br>
   
   <li>
     <label>Ubicación:</label>
     <input type="text" id="mail" name="ubicacion">
-  </li>
+  </li> <br>
 
-  <h3> Tipo de contrato: </h3> 
+  <li>
+  Tipo de contrato: 
   <INPUT TYPE="radio" VALUE="tipo_contrato">Temporal
-
-<INPUT TYPE="radio" NAME="tipo_contrato">Fijo
-
+<INPUT TYPE="radio" NAME="tipo_contrato"> Fijo 
+</li> <br>
+ 
   <li>
     <label>Horario:</label>
     <input type="text" id="name" name="horario">
-  </li>
+  </li> <br>
 
   <li>
     <label>Correo al que se debe enviar los curriculum:</label>
     <input type="email" id="mail" name="correo">
-  </li>
+  </li> <br>
 
   <li>
     <label> Persona de contacto:</label>
     <input type="text"  name="contacto">
-  </li>
- 
-
+  </li> <br>
  </ul>
+
+ <INPUT TYPE="button" NAME="actualizar" VALUE="Enviar información">
 </form>
-    
+</section>
+
 </body>
 </html>
