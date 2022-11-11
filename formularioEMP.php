f7454b630b2a6841e237b61cce1f81b82720c2b6
ghostpastel <2190029@ipisa.edu.do>
2022-11-10 21:24:55 -0400

    Enlazada hoja de estilo a formularioVANC.php

--- formularioEMP.php
+++ formularioEMP.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="formularioEMP.css">
+  <link rel="stylesheet" href="http://localhost/practica_equipo2/css/formularioEMP.css">
   <title>Formulario empresa</title>
 </head>
 <body>
--- formularioVANC.php
+++ formularioVANC.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="http://localhost/practica_equipo2/css/formularioVANC.css">
     <title>Formulario de vacantes</title>
-    <link rel="stylesheet" type="text/css" href="css/formularioVANC.css">
 </head>
 
 <body>
@@ -46,11 +46,6 @@
 <br>
  
 
-<<<<<<< HEAD
-
-  <li>
-=======
->>>>>>> e3b0f47e733fec5697d7c38a457e73e758cfe60f
     <label>Horario:</label>
     <input type="text" id="name" name="horario">
  <br>
