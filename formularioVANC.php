bc89ee15f1b2123a58e164fbb241f8776119d9fa
yli18 <2190021@ipisa.edu.do>
2022-11-10 22:23:37 -0400

    Cambios en el formulario vacantes

--- formularioVANC.php
+++ formularioVANC.php
@@ -14,44 +14,44 @@
 <form action="conexion_formularioVan.php " method="post">
  
     <label>Nombre de la empresa:</label>
-    <input type="text" id="name" name="nombre_empresa"> <br>  
+    <input type="text"  class="field" name="nombre_empresa"> <br>  
 
   
 
     <label>Nombre de la vacante:</label>
-    <input type="text" id="name" name="vacante_nombre"> <br>
+    <input type="text" class="field" name="vacante_nombre"> <br>
 
  
     Funciones o perfil del puesto: <br>
-    <textarea  COLS="40" ROWS="4" id="h" name="funciones_puesto"></textarea> <br>
+    <textarea  COLS="40" ROWS="4" id="h" class="field" name="funciones_puesto"></textarea> <br>
 
 
     <label>Sueldo:</label>
-    <input type="text" id="name" name="sueldo"> <br>
+    <input type="text" class="field" name="sueldo"> <br>
   
  
     <label>Ubicación:</label>
-    <input type="text" id="mail" name="ubicacion"> <br>
+    <input type="text"  class="field" name="ubicacion"> <br>
 
 
   Tipo de contrato: 
-  <INPUT TYPE="radio" NAME="tipo_contrato">Temporal
- <INPUT TYPE="radio" NAME="tipo_contrato"> Fijo  
+  <INPUT TYPE="radio" class="field" NAME="tipo_contrato">Temporal
+ <INPUT TYPE="radio" class="field" NAME="tipo_contrato"> Fijo  
  <br>
  
 
     <label>Horario:</label>
-    <input type="text" id="name" name="horario"> <br>
+    <input type="text" class="field" name="horario"> <br>
 
     <label>Correo al que se debe enviar los curriculum:</label>
-    <input type="email" id="mail" name="correo"> <br>
+    <input type="email" id="mail" class="field" name="correo"> <br>
 
  
     <label> Persona de contacto:</label>
-    <input type="text"  name="Persona_contacto"> <br>
+    <input type="text" class="field" name="Persona_contacto"> <br>
  
     <label> Telefono del contacto:</label>
-    <input type="text"  name="telefono_contacto"> <br>
+    <input type="text" class="field" name="telefono_contacto"> <br>
 
  <INPUT TYPE="submit" NAME="enviar" VALUE="Enviar información">
 </form>
