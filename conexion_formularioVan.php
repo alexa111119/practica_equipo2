8c8f5830dc74753c5768ea3fd28e9abe8dbe7609
alexac <2190006@ipisa.edu.do>
2022-11-17 10:55:37 -0400

    cambios en vacantes

--- conexion_formularioVan.php
+++ conexion_formularioVan.php
@@ -18,21 +18,15 @@ $conexion = mysqli_connect('localhost', 'root', '04272931', 'practica_ipi')
     $Persona_contacto = $_POST['Persona_contacto']?? null;
     $telefono_contacto = $_POST['telefono_contacto']?? null;
 
-    $idEmpresas= "select idEmpresas from empresas where Nombre_Empresa = '$nombre_empresa' ";
-    $validarId = mysqli_query($mysqli, $idEmpresas); 
-    
-    if(mysqli_num_rows($validarId) > 0 ) {
-        $resultado = $mysqli->query($idEmpresas);
-        $row = mysqli_fetch_row($resultado);
-        $variable_id = $row[0];
-    }
-    $consulta = "INSERT INTO vacantes(Nombre_empresa, Nombre_vacante, Funciones, Sueldo, Ubicacion, Tipo_contrato, Horario, Correo, Persona_contacto, Telefono,) 
+        $consulta = "INSERT INTO vacantes(Nombre_empresa, Nombre_vacante, Funciones, Sueldo, Ubicacion, Tipo_contrato, Horario, Correo, Persona_contacto, Telefono) 
     VALUES('$nombre_empresa', '$vacante_nombre', '$funciones_puesto', '$sueldo', '$ubicacion', ' $tipo_contrato', '$horario', '$correo', '$Persona_contacto', '$telefono_contacto')";
   
     mysqli_query($conexion, $consulta);
     mysqli_close($conexion);
+    }
+    
    
-  }
+  
 echo "Connected successfully";
 
 ?>
